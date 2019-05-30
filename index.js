d5b73f22ed61f68eb39c74365e35c6a210071f23
LouisM <louis_mbuyu@unigroup.com>
2019-05-30 18:38:46 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res) => {
+    return res.json({message: "Hello World"});
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log("Listening on: ",PORT);
+});
